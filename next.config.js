f968262e5dbad01711e0abef1f657a95e64d11ac
pbassham <pbassham@gmail.com>
2022-12-08 12:10:10 -0600

    update packages. /app dir doesnt work with chakra

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const NextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
+  experimental: {
+    appDir: false,
+  },
 }
+module.exports = NextConfig
