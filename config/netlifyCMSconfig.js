7a85ec8a1ac25200de2ee72a013c3d100b468aa9
pbassham <pbassham@gmail.com>
2022-12-09 17:17:17 -0600

    identity

--- config/netlifyCMSconfig.js
+++ config/netlifyCMSconfig.js
@@ -39,7 +39,7 @@ export default {
     name: "github",
     repo: "pbassham/philbassham.com",
     identity_url: "https://golden-manatee-569b12.netlify.app/.netlify/identity",
-    gateway_url: "https://golden-manatee-569b12.netlify.app/.netlify/git",
+    // gateway_url: "https://golden-manatee-569b12.netlify.app/.netlify/git",
     branch: branch, //"master"
   },
   //   load_config_file: false,
