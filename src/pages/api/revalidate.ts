bc9bfa328bc2a21a02794dd4288c7a4e4ec96353
pbassham <pbassham@gmail.com>
2023-01-10 16:26:03 -0600

    revalidate index pages on new post too

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate(`/${slug}`)
+    await res.revalidate(`/`)
+    await res.revalidate(`/projects`)
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
